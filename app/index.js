0cc9b542920b18417b61b33dcf44fdb71a0069b2
Dominic PrawnStar Bauer <dominic.g.bauer@gmail.com>
2018-10-12 07:17:21 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+var App = require('./components/App');
+
+// What makes up a component
+// state
+// lifecycle events
+// UI
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
